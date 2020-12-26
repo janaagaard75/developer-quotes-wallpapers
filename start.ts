0389a6436c43ea86c455b361b856258f6dfbc84d
Jan Aagaard <jan@aagaard.net>
2020-12-26 20:19:44 +0100

    Introduce getHtml

--- start.ts
+++ start.ts
@@ -2,6 +2,21 @@ import fs from "fs";
 import path from "path";
 import puppeteer from "puppeteer";
 
+interface Quote {
+  author: string;
+  text: string;
+  year: number;
+}
+
+const getHtml = (quote: Quote): string => {
+  const template = fs.readFileSync("template.html", { encoding: "utf-8" });
+  const html = template
+    .replace("{{author}}", quote.author)
+    .replace("{{text}}", quote.text)
+    .replace("{{year}}", quote.year.toString());
+  return html;
+};
+
 const main = async () => {
   const browser = await puppeteer.launch({ headless: true });
   try {
@@ -12,7 +27,11 @@ const main = async () => {
       width: 2560,
     });
 
-    const html = fs.readFileSync("template.html", { encoding: "utf-8" });
+    const html = getHtml({
+      author: "Sandi Metz",
+      text: "Duplication is far cheaper than the wrong abstraction.",
+      year: 2014,
+    });
     await page.setContent(html);
 
     const screenshotFilePath = path.join(__dirname, "demo.png");
