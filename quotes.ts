c85360f1a5a7a557267ece3034596e08a82ca56f
Jan Aagaard <jan@aagaard.net>
2020-12-26 21:17:08 +0100

    Move quotes to quotes.ts

---
+++ quotes.ts
@@ -0,0 +1,41 @@
+export interface Quote {
+  author: string;
+  text: string;
+  year?: number;
+}
+
+export const quotes: Array<Quote> = [
+  {
+    author: "Sandi Metz",
+    text: "Duplication is far cheaper than the wrong abstraction.",
+    year: 2014,
+  },
+  {
+    author: "Linus Torvalds",
+    text: "Given enough eyeballs, all bugs are shallow.",
+    year: 1999,
+  },
+  {
+    author: "Brian W. Kernighan",
+    text:
+      "Everyone knows that debugging is twice as hard as writing a program in the first place. So if you're as clever as you can be when you write it, how will you ever debug it?",
+  },
+  {
+    author: "Josh Bloch",
+    text: "APIs should be easy to use and hard to misuse.",
+  },
+  {
+    author: "Tony Hoare",
+    text:
+      "There are two ways of constructing a software design: One way is to make it so simple that there are obviously no deficiencies, and the other way is to make it so complicated that there are no obvious deficiencies. The first method is far more difficult.",
+  },
+  {
+    author: "Linus Torvalds",
+    text:
+      "The difference between a bad programmer and a good one is whether he considers his code or his data structures more important. Bad programmers worry about the code. Good programmers worry about data structures and their relationships.",
+  },
+  {
+    author: "Brian W. Kernighan",
+    text: "Don’t comment bad code&mdash;rewrite it.",
+  },
+];
--- start.ts
+++ start.ts
@@ -1,50 +1,7 @@
 import { promises as fs } from "fs";
 import path from "path";
 import puppeteer, { Browser, Page } from "puppeteer";
-
-interface Quote {
-  author: string;
-  text: string;
-  year?: number;
-}
-
-const getQuotes = (): Array<Quote> => {
-  return [
-    {
-      author: "Sandi Metz",
-      text: "Duplication is far cheaper than the wrong abstraction.",
-      year: 2014,
-    },
-    {
-      author: "Linus Torvalds",
-      text: "Given enough eyeballs, all bugs are shallow.",
-      year: 1999,
-    },
-    {
-      author: "Brian W. Kernighan",
-      text:
-        "Everyone knows that debugging is twice as hard as writing a program in the first place. So if you're as clever as you can be when you write it, how will you ever debug it?",
-    },
-    {
-      author: "Josh Bloch",
-      text: "APIs should be easy to use and hard to misuse.",
-    },
-    {
-      author: "Tony Hoare",
-      text:
-        "There are two ways of constructing a software design: One way is to make it so simple that there are obviously no deficiencies, and the other way is to make it so complicated that there are no obvious deficiencies. The first method is far more difficult.",
-    },
-    {
-      author: "Linus Torvalds",
-      text:
-        "The difference between a bad programmer and a good one is whether he considers his code or his data structures more important. Bad programmers worry about the code. Good programmers worry about data structures and their relationships.",
-    },
-    {
-      author: "Brian W. Kernighan",
-      text: "Don’t comment bad code—rewrite it.",
-    },
-  ];
-};
+import { Quote, quotes } from "./quotes";
 
 const getBrowserPage = async (browser: Browser): Promise<Page> => {
   const page = await browser.newPage();
@@ -86,7 +43,6 @@ const main = async () => {
   const template = await fs.readFile("template.html", { encoding: "utf-8" });
   try {
     const page = await getBrowserPage(browser);
-    const quotes = getQuotes();
     for (let i = 0; i < quotes.length; i++) {
       const html = await getHtml(template, quotes[i]);
       await page.setContent(html);
