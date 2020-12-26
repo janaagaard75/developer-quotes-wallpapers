84747f20d7710129cc6e0d76a85b034a0839c2c0
Jan Aagaard <jan@aagaard.net>
2020-12-26 21:30:30 +0100

    Sort quotes by author

--- quotes.ts
+++ quotes.ts
@@ -5,12 +5,12 @@ export interface Quote {
 
 export const quotes: Array<Quote> = [
   {
-    author: "Sandi Metz",
-    text: "Duplication is far cheaper than the wrong abstraction.",
+    author: "?",
+    text: "Singleton is just a fancy name for global.",
   },
   {
-    author: "Linus Torvalds",
-    text: "Given enough eyeballs, all bugs are shallow.",
+    author: "Brian W. Kernighan",
+    text: "Don’t comment bad code&mdash;rewrite it.",
   },
   {
     author: "Brian W. Kernighan",
@@ -22,9 +22,8 @@ export const quotes: Array<Quote> = [
     text: "APIs should be easy to use and hard to misuse.",
   },
   {
-    author: "Tony Hoare",
-    text:
-      "There are two ways of constructing a software design: One way is to make it so simple that there are obviously no deficiencies, and the other way is to make it so complicated that there are no obvious deficiencies. The first method is far more difficult.",
+    author: "Linus Torvalds",
+    text: "Given enough eyeballs, all bugs are shallow.",
   },
   {
     author: "Linus Torvalds",
@@ -32,7 +31,12 @@ export const quotes: Array<Quote> = [
       "The difference between a bad programmer and a good one is whether he considers his code or his data structures more important. Bad programmers worry about the code. Good programmers worry about data structures and their relationships.",
   },
   {
-    author: "Brian W. Kernighan",
-    text: "Don’t comment bad code&mdash;rewrite it.",
+    author: "Sandi Metz",
+    text: "Duplication is far cheaper than the wrong abstraction.",
+  },
+  {
+    author: "Tony Hoare",
+    text:
+      "There are two ways of constructing a software design: One way is to make it so simple that there are obviously no deficiencies, and the other way is to make it so complicated that there are no obvious deficiencies. The first method is far more difficult.",
   },
 ];
