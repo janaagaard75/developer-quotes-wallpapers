aafac13b4d1057e5db468700ec1bda3fe9b63ceb
Jan Aagaard <jan@aagaard.net>
2021-01-02 21:01:43 +0100

    Introduce WallpaperGenerator

---
+++ src/WallpaperGenerator.ts
@@ -0,0 +1,82 @@
+import path from "path";
+import { Browser, Page } from "puppeteer";
+import { Quote } from "./quotes";
+
+export class WallpaperGenerator {
+  private constructor(
+    private template: string,
+    private wallpapersFolderName: string,
+    private screenHeight: number,
+    private screenWidth: number,
+    private backgroundSrc: string
+  ) {}
+
+  public static async createInstance(
+    browser: Browser,
+    template: string,
+    wallpapersFolderName: string,
+    screenHeight: number,
+    screenWidth: number,
+    backgroundSrc: string
+  ): Promise<WallpaperGenerator> {
+    const instance = new WallpaperGenerator(
+      template,
+      wallpapersFolderName,
+      screenHeight,
+      screenWidth,
+      backgroundSrc
+    );
+    instance.page = await instance.getBrowserPage(browser);
+    return instance;
+  }
+
+  private page!: Page;
+
+  public async generate(quote: Quote, quoteIndex: number) {
+    const html = await this.getHtml(quote);
+    await this.page.setContent(html);
+    const screenshotFilePath = this.getScreenshotFilePath(
+      this.wallpapersFolderName,
+      quoteIndex
+    );
+    await this.page.screenshot({ path: screenshotFilePath });
+  }
+
+  private async getBrowserPage(browser: Browser): Promise<Page> {
+    const page = await browser.newPage();
+    await page.setViewport({
+      deviceScaleFactor: 1,
+      height: this.screenHeight,
+      width: this.screenWidth,
+    });
+    return page;
+  }
+
+  private async getHtml(quote: Quote): Promise<string> {
+    let html = this.template
+      .replace("{{backgroundSrc}}", this.backgroundSrc)
+      .replace("{{title}}", quote.title ?? "")
+      .replace("{{text}}", quote.text)
+      .replace("{{author}}", quote.author);
+    return html;
+  }
+
+  private getScreenshotFilePath(
+    wallpapersFolderName: string,
+    quoteIndex: number
+  ): string {
+    const screenshotFilePath = path.join(
+      __dirname,
+      "..",
+      wallpapersFolderName,
+      `quote-${WallpaperGenerator.padNumber(quoteIndex + 1, 2)}.png`
+    );
+    return screenshotFilePath;
+  }
+
+  private static padNumber(value: number, size: number): string {
+    const paddedValue = "000000000" + value;
+    const paddedAndTrimmedValue = paddedValue.substr(-size);
+    return paddedAndTrimmedValue;
+  }
+}
--- src/wallpapers.ts
+++ src/wallpapers.ts
@@ -1,52 +1,15 @@
 import { promises as fs } from "fs";
 import svgToMiniDataURI from "mini-svg-data-uri";
-import path from "path";
-import puppeteer, { Browser, Page } from "puppeteer";
+import puppeteer from "puppeteer";
 import { BackgroundGenerator } from "./BackgroundGenerator";
-import { Quote, quotes } from "./quotes";
+import { quotes } from "./quotes";
 import { screenHeight, screenWidth, wallpapersFolderName } from "./settings";
+import { WallpaperGenerator } from "./WallpaperGenerator";
 
 const backgroundSvg = new BackgroundGenerator().getSvgString();
 const backgroundSrc = svgToMiniDataURI(backgroundSvg);
 
-const getBrowserPage = async (browser: Browser): Promise<Page> => {
-  const page = await browser.newPage();
-  await page.setViewport({
-    deviceScaleFactor: 1,
-    height: screenHeight,
-    width: screenWidth,
-  });
-  return page;
-};
-
-const getHtml = async (template: string, quote: Quote): Promise<string> => {
-  let html = template
-    .replace("{{backgroundSrc}}", backgroundSrc)
-    .replace("{{title}}", quote.title ?? "")
-    .replace("{{text}}", quote.text)
-    .replace("{{author}}", quote.author);
-  return html;
-};
-
-const padNumber = (value: number, size: number): string => {
-  const paddedValue = "000000000" + value;
-  const paddedAndTrimmedValue = paddedValue.substr(-size);
-  return paddedAndTrimmedValue;
-};
-
-const getScreenshotFilePath = (
-  wallpapersFolderName: string,
-  quoteIndex: number
-): string => {
-  const screenshotFilePath = path.join(
-    __dirname,
-    "..",
-    wallpapersFolderName,
-    `quote-${padNumber(quoteIndex + 1, 2)}.png`
-  );
-  return screenshotFilePath;
-};
-
+// Can't use forEach with a async callback. This is the workaround. https://codeburst.io/javascript-async-await-with-foreach-b6ba62bbf404
 const asyncForEach = async <T>(
   array: Array<T>,
   callback: (value: T, index: number, array: Array<T>) => Promise<void>
@@ -60,20 +23,20 @@ const main = async () => {
   await fs.mkdir(wallpapersFolderName, { recursive: true });
 
   const browser = await puppeteer.launch({ headless: true });
-  const template = await fs.readFile("src/template.html", {
-    encoding: "utf-8",
-  });
   try {
-    const page = await getBrowserPage(browser);
-    // Can't use forEach with a async callback. https://codeburst.io/javascript-async-await-with-foreach-b6ba62bbf404
+    const template = await fs.readFile("src/template.html", {
+      encoding: "utf-8",
+    });
+    const wallpaperGenerator = await WallpaperGenerator.createInstance(
+      browser,
+      template,
+      wallpapersFolderName,
+      screenHeight,
+      screenWidth,
+      backgroundSrc
+    );
     await asyncForEach(quotes, async (quote, quoteIndex) => {
-      const html = await getHtml(template, quote);
-      await page.setContent(html);
-      const screenshotFilePath = getScreenshotFilePath(
-        wallpapersFolderName,
-        quoteIndex
-      );
-      await page.screenshot({ path: screenshotFilePath });
+      await wallpaperGenerator.generate(quote, quoteIndex);
     });
   } finally {
     await browser.close();
