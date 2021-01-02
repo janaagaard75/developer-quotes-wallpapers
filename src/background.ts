f2e09c918bdaf83d8cb44399b596dc12d3cd3db2
Jan Aagaard <jan@aagaard.net>
2021-01-02 19:12:59 +0100

    Convert to a class

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,4 @@
-import { SVG } from "@svgdotjs/svg.js";
+import { Svg, SVG } from "@svgdotjs/svg.js";
 import fs from "fs";
 import {
   screenHeight,
@@ -13,53 +13,81 @@ const { registerWindow } = require("@svgdotjs/svg.js");
 
 fs.mkdirSync(wallpapersFolderName, { recursive: true });
 
-const svgWindow = createSVGWindow();
-registerWindow(svgWindow, svgWindow.document);
-const svgCanvas = SVG(svgWindow.document.documentElement as SVGSVGElement);
-svgCanvas.size(screenWidth, screenHeight);
-
-const getColor = (i: number, j: number): string => {
-  const top = "#333";
-  const left = "#222";
-  const right = "#111";
-
-  if (j % 3 === 0) {
-    return top;
+class BackgroundGenerator {
+  constructor() {
+    const svgWindow = createSVGWindow();
+    registerWindow(svgWindow, svgWindow.document);
+    this.svgCanvas = SVG(svgWindow.document.documentElement as SVGSVGElement);
+    this.svgCanvas.size(screenWidth, screenHeight);
   }
 
-  if ((i + 2 * j) % 2 === 0) {
-    return left;
-  }
+  private svgCanvas: Svg;
 
-  return right;
-};
-
-const round = (value: number): number => {
-  return Number.parseFloat(value.toFixed(2));
-};
-
-for (let i = 0; i < screenWidth / x; i++) {
-  for (let j = -1; j < screenHeight / y; j++) {
-    if ((i + j) % 2 === 0) {
-      svgCanvas
-        .polygon(`0,0 ${round(x)},${round(y)} 0,${round(2 * y)}`)
-        .move(round(i * x), round(j * y))
-        .fill(getColor(i, j))
-        .stroke({
-          color: getColor(i, j),
-          width: strokeWidth,
-        });
-    } else {
-      svgCanvas
-        .polygon(`0,${round(y)} ${round(x)},0 ${round(x)},${round(2 * y)}`)
-        .move(round(i * x), round(j * y))
-        .fill(getColor(i, j))
-        .stroke({
-          color: getColor(i, j),
-          width: strokeWidth,
-        });
+  public getSvgString(): string {
+    for (let i = 0; i < screenWidth / x; i++) {
+      for (let j = -1; j < screenHeight / y; j++) {
+        if ((i + j) % 2 === 0) {
+          this.svgCanvas
+            .polygon(
+              `0,0 ${BackgroundGenerator.round(x)},${BackgroundGenerator.round(
+                y
+              )} 0,${BackgroundGenerator.round(2 * y)}`
+            )
+            .move(
+              BackgroundGenerator.round(i * x),
+              BackgroundGenerator.round(j * y)
+            )
+            .fill(BackgroundGenerator.getColor(i, j))
+            .stroke({
+              color: BackgroundGenerator.getColor(i, j),
+              width: strokeWidth,
+            });
+        } else {
+          this.svgCanvas
+            .polygon(
+              `0,${BackgroundGenerator.round(y)} ${BackgroundGenerator.round(
+                x
+              )},0 ${BackgroundGenerator.round(x)},${BackgroundGenerator.round(
+                2 * y
+              )}`
+            )
+            .move(
+              BackgroundGenerator.round(i * x),
+              BackgroundGenerator.round(j * y)
+            )
+            .fill(BackgroundGenerator.getColor(i, j))
+            .stroke({
+              color: BackgroundGenerator.getColor(i, j),
+              width: strokeWidth,
+            });
+        }
+      }
     }
+
+    const svgString = this.svgCanvas.svg();
+    return svgString;
+  }
+
+  private static getColor(i: number, j: number): string {
+    const top = "#333";
+    const left = "#222";
+    const right = "#111";
+
+    if (j % 3 === 0) {
+      return top;
+    }
+
+    if ((i + 2 * j) % 2 === 0) {
+      return left;
+    }
+
+    return right;
+  }
+
+  private static round(value: number): number {
+    return Number.parseFloat(value.toFixed(2));
   }
 }
 
-fs.writeFileSync("wallpapers/background.svg", svgCanvas.svg());
+const svgString = new BackgroundGenerator().getSvgString();
+fs.writeFileSync("wallpapers/background.svg", svgString);
