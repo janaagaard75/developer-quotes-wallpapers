d31a08574a754a60a0e97eda97e887ec9f779a2d
Jan Aagaard <jan@aagaard.net>
2025-04-03 09:21:29 +0200

    Rename to screenshotFullName

--- src/WallpaperGenerator.ts
+++ src/WallpaperGenerator.ts
@@ -43,11 +43,11 @@ export class WallpaperGenerator {
     await this.page.setContent(html, { waitUntil: "networkidle0" });
     const wallpaperFolderName = this.getWallpaperFolderName();
     await fs.mkdir(wallpaperFolderName, { recursive: true });
-    const screenshotFilePath = this.getScreenshotFilePath(
+    const screenshotFullName = this.getScreenshotFullName(
       wallpaperFolderName,
       fileName,
     );
-    await this.page.screenshot({ path: screenshotFilePath });
+    await this.page.screenshot({ path: screenshotFullName });
   }
 
   private async getBrowserPage(browser: Browser): Promise<Page> {
@@ -80,7 +80,7 @@ export class WallpaperGenerator {
     return wallpaperFolderName;
   }
 
-  private getScreenshotFilePath(
+  private getScreenshotFullName(
     wallpaperFolderName: string,
     fileName: string,
   ): string {
